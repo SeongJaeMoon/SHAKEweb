07cd6281082549786f191bda80d7ca3f29c3757c
SeongJaeMoon <seongjae.m@gmail.com>
2017-09-25 20:05:59 +0900

    new

---
+++ js/shake.js
@@ -0,0 +1,12 @@
+<!DOCTPYE html>
+<html lang ="en">
+<head>
+  <meta charset="UTF-8">
+  <title>NAMSEOUL SHUTTLE</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+  <body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jqeury/1.12.4/jqeury.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+  </body>
+</html>
